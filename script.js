4d87f2e0bf34d5f704cb96daa8e576a051ae3975
roluochKe <rasholuoch@gmail.com>
2021-11-01 21:46:54 +0300

    post creation modal

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ const newsFeedPage = document.querySelector('.feeds-page')
 const loginModal = document.querySelector('.login-modal')
 const modalX = document.querySelector('.login-modal i')
 const loginFormBtn = document.querySelector('.login-form-btn')
+const postBtn = document.querySelector('.post-btn')
+const modalWrapper = document.querySelector('.modal-wrapper')
+const modal = document.querySelector('.modal')
+const postModalX = document.querySelector('.modal-header i')
+const modalPostBtn = document.querySelector('.modal-header button')
+const modalFooterPlus = document.querySelector('.modal-footer span')
+const modalInput = document.querySelector('.modal-input')
 /**************************************/
 /**************************************/
 
@@ -49,5 +56,39 @@ loginFormBtn.addEventListener('click', () => {
     newsFeedPage.style.display = 'block'
   } else {
     loginModal.style.display = 'block'
+  }
+})
+
+// News feed page
+// Post modal
+postBtn.addEventListener('click', () => {
+  modal.style.display = 'block'
+  modalWrapper.classList.add('modal-wrapper-display')
+})
+
+const changeOpacity = (x) => {
+  modalPostBtn.style.opacity = x
+  modalFooterPlus.style.opacity = x
+}
+
+postModalX.addEventListener('click', () => {
+  modal.style.display = 'none'
+  modalWrapper.classList.remove('modal-wrapper-display')
+
+  if (modalInput.value !== '') {
+    modalInput.value = ''
+    changeOpacity(0.5)
+  }
+})
+
+modalInput.addEventListener('keypress', (e) => {
+  if (e.target.value !== '') {
+    changeOpacity(1)
+  }
+})
+
+modalInput.addEventListener('blur', (e) => {
+  if (e.target.value === '') {
+    changeOpacity(0.5)
   }
 })
